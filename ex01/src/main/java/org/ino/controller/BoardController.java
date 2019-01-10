80ec0ec4bc3abcac59fce87a766921b7b932de13
최인호 <ino0319@gmail.com>
2019-01-10 17:03:52 +0900

    Paging

--- ex01/src/main/java/org/ino/controller/BoardController.java
+++ ex01/src/main/java/org/ino/controller/BoardController.java
@@ -1,18 +1,22 @@
 package org.ino.controller;
 
 import javax.inject.Inject;
+import javax.print.attribute.standard.Severity;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.ino.domain.BoardVO;
 import org.ino.domain.Criteria;
+import org.ino.domain.PageMaker;
 import org.ino.service.BoardService;
 
 @Controller
@@ -84,8 +88,23 @@ public class BoardController {
 
 	@RequestMapping(value = "/listCri", method = RequestMethod.GET)
 	public void listAll(Criteria cri, Model model) throws Exception {
-		
+
 		logger.info("show list Page with Criteria..........");
 		model.addAttribute("list", service.listCriteria(cri));
+	}
+
+	@RequestMapping(value = "/listPage", method = RequestMethod.GET)
+	public void listPage(@ModelAttribute("cri")Criteria cri, Model model) throws Exception {
+
+		logger.info(cri.toString());
+
+		model.addAttribute("list", service.listCriteria(cri));
+		PageMaker pageMaker = new PageMaker();
+		pageMaker.setCri(cri);
+		//pageMaker.setTotalCount(131);
+		
+		pageMaker.setTotalCount(service.listCountCriteria(cri));
+
+		model.addAttribute("pageMaker", pageMaker);
 	}
 }
