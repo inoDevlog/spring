66af52ef2f81d144dd61b6f7dff42952a67c2cba
최인호 <ino0319@gmail.com>
2019-01-25 18:01:34 +0900

    test

--- ex02/src/main/java/org/ino/controller/SampleController.java
+++ ex02/src/main/java/org/ino/controller/SampleController.java
@@ -1,7 +1,9 @@
 package org.ino.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ino.domain.SampleVO;
-import org.springframework.context.event.SmartApplicationListener;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,22 @@ public class SampleController {
 		vo.setMno(123);
 
 		return vo;
+	}
+
+	@RequestMapping("/sendList")
+	public List<SampleVO> sendList() {
+
+		List<SampleVO> list = new ArrayList<>();
+		
+		for (int i = 0; i < 10; i++) {
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("길동");
+			vo.setLastName("홍");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		return list;
+
 	}
 
 }
